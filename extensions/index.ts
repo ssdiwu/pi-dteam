da1d11c1cf5a1f512eff6adfa02b943949a7245f
ssdiwu <467012623@qq.com>
2026-06-01 02:17:18 +0800

    修复：扩展入口路径 src/extension → src/P4

--- extensions/index.ts
+++ extensions/index.ts
@@ -1,6 +1,6 @@
 /**
  * pi-dteam 扩展入口文件
- * 重新导出 src/extension/index.ts 的内容
+ * 重新导出 src/P4/index.ts 的内容
  */
 
-export { default } from "../src/extension/index.ts";
+export { default } from "../src/P4/index.ts";
